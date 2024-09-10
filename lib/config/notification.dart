2f08a3638f24a4e2ff25e4cd0ef94d53818847b4
ebinpeter <ebinpeter94@gmail.com>
2024-09-10 18:24:19 +0530

    dddddeedsssssdfd

---
+++ lib/config/notification.dart
@@ -0,0 +1,21 @@
+// class Pushnotification {
+//   static Future<String>getAccessToken()async{
+//     final serviceAccounjson =
+//     {
+//       "type": "service_account",
+//       "project_id": "mychat-5b639",
+//       "private_key_id": "a36aae0fad5f67cb454aa0c6fbe74d17bb4bb0a2",
+//       "private_key": "-----BEGIN PRIVATE KEY-----\nMIIEugIBADANBgkqhkiG9w0BAQEFAASCBKQwggSgAgEAAoIBAQCg3fXktsksXMFc\nqMWetMQYsxgI2bOF4hB5GUu42QapFrEOZKpyB13EB0W5LFePWyAi/jCwbJalgJn1\nbet6888pyMWNDtAls86UJHyybUTtoX5m7ufSTBVtBE9XfNgrZec0luIS466OTJts\nwt5qykXuTLpoyQAecy/6TceJGCk7kS71hloU9pA21GA9QD1ekv2ygJDVpeB/XWZK\nEezHzwdIpNaqGZcAxlkGcbEbbRPx2+ek8bm+vmZRCkJXpk3KubNdzdMR+YpF/e+h\nriJqaKh6RkcCkeEEKaqQ4DHvecUzkyYQv7FfDXAC9V4o62a03v/WcIEqYFkk7Ewk\n8M6Qx/JrAgMBAAECgf9o8GJ30MVrKxb4WyCPZ1yEwE/tdhamP9J/LQE4lFrD8wmN\nWh3WRPfl6NiS81orKkbMjQU2EaA0VrAYapwymNmLaG12lzMxZRpdiW/O9XkBi1fp\nze9sQDW56BXZ/JcHuHIa3FKd/+je6xp42KY/A7Sav3La6EQrmPizUAnaZOiA8iud\nE8Aq68yo93nMTQsTPJlACGJ17n4WWRo2ttklYrFatCH4M1WgryyELqtXjUp0lqzi\nNmhiqUGgShE167hxmcg9uahHlVOdQ6IvVEknFs1koyuLIXi9+1upZtB+mcyvczX2\nmfNPMrLckU7Y1Lmdds1u/8bn0q4LjmEqUKa/t/ECgYEA1PtEPGousPKbqg8vr8Ed\niilsBGVpJDBPIvhFtQZHMa1SWgmaWQBbNRmnSh++5ByNMnMgIhTLLIWlYO48GKw5\nF/lcvU/yJBxbuyP+lTcKtPa/IBzGPxwGqUgfS0Ck5AiKgZ7vOe63tlzBKHu6K7JP\nG9EUSJoGrJvcOOrIWzzvbAkCgYEAwVv8k1PoHTnZdihGcvqJ4SkSCupjIXQXK3be\nhJ3qMHLbtFBGDr55Pa1k8a0pYminauPPnOEHs/0k96OvS43qCHkf/sGo9fSbsYDP\nBZBNKG1kkGSRW5YyLkkfNo4A4kMtl4wpgk5h+fDo1f/B+3nXTGrWiGwsUktiM9iE\ntSVKb9MCgYBM9icoNa59no2FnEs9FWmZPXKc/BBMVXCjUFfQHF6potn5UzMFKk3L\nGGQNMv+F6pNTQarA5gatJfYm2q+4srNG6IMMndEAH4c+Trl2LheeYPObpQo2T+hQ\nT5GPXeK3zCZQxJ/prUuRiF78RAl4NMzFXn4X5iC3SvL/VkfkQFa8EQKBgD6dskpn\nhqhIPpLT8hRz68idizk7tAR2WrZ5lQ+vL9Kmr11SDY3iwtJlX2YgJGG48FZeFbKA\n8QSpepozSKYX++zohKL0Ren4X574zpHHrCTmYAWee7AEEJYKmfAJxmF/Tblg62RG\nS9Yr3MrIOyJbz4vwoS6mkSfJP7D4celTfSp/AoGAPxBobgYcXmwI8gv6eLO0lBTV\nJSUzHvJSPJfgNNrYc5f/b42RVjka+UrW7veErjbdJyY5pUfUNEVbbmi6NejLHrgM\n0df6okgiv7ZSJM+V/mNEIzxc8w8ZEnRahT9wcgmUEnyGOWm86PshMbjU214Tslnk\nRj5SczIvRmOAKKqjEOU=\n-----END PRIVATE KEY-----\n",
+//       "client_email": "mychattick@mychat-5b639.iam.gserviceaccount.com",
+//       "client_id": "100664516761695274577",
+//       "auth_uri": "https://accounts.google.com/o/oauth2/auth",
+//       "token_uri": "https://oauth2.googleapis.com/token",
+//       "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
+//       "client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/mychattick%40mychat-5b639.iam.gserviceaccount.com",
+//       "universe_domain": "googleapis.com"
+//     };
+//   }
+//   static sendNotification()async{
+//
+//   }
+// }
